326a202b9f3a68e7c7900754d900ca575af69c0c
Keith Vickery <keithwv@hotmail.com>
2025-08-20 23:13:59 -0600

    END OF SECTION 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,17 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseAuthorization();
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 
 app.MapControllers();
 
---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react"
+import type { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
+import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import NavBar from "./NavBar";
+
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const palleteType = darkMode ? 'dark' : 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212',
+      }
+    }
+  });
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  const addProduct = () => {
+    setProducts(prevState => [
+      ...prevState,
+      {
+        id: prevState.length + 1,
+        name: 'product' + (prevState.length + 1),
+        price: (prevState.length * 100) + 100,
+        quantityInStock: 100,
+        description: 'New product',
+        pictureUrl: 'https://picsum.photos/200',
+        type: 'Default',
+        brand: 'Default',
+      }
+    ]);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar toggleDarkMode={toggleDarkMode} />
+      <Box
+        sx={{
+          minHeight: '100vh',
+          background: darkMode ? 'radial-gradient(circle, #1e3aBA,#111B27)' : 'radial-gradient(circle, #baecf9, #f0f9ff',
+          py: 6
+        }}>
+        <Container maxWidth="xl" sx={{ mt: 8 }}>
+          <Catalog products={products} addProduct={addProduct} />
+        </Container>
+      </Box >
+    </ThemeProvider>
+  )
+}
+
+export default App
